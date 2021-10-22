6e75f203e2601e21a49874aa9a9fb967ef90e99c
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-22 11:53:54 -1000

    f

--- app.py
+++ app.py
@@ -17,27 +17,6 @@ def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	return render_template("index.html")
 
-def find_list_resources (tag, attribute,soup, url):
-	for x in soup.findAll(tag):
-		try:
-			x[attribute] = urllib.urljoin(url, x[attribute])
-		except KeyError:
-			pass
-	return soup
-
-# soup = BeautifulSoup(html)
-
-# image_scr = find_list_resources('img',"src",soup)   
-# scipt_src = find_list_resources('script',"src",soup)    
-# css_link = find_list_resources("link","href",soup)
-# video_src = find_list_resources("video","src",soup)         
-# audio_src = find_list_resources("audio","src",soup) 
-# iframe_src = find_list_resources("iframe","src",soup)
-# embed_src = find_list_resources("embed","src",soup)
-# object_data = find_list_resources("object","data",soup)         
-# soruce_src = find_list_resources("source","src",soup)    
-
-
 @app.route("/api")
 def api():
 	url = request.args.get('url')
@@ -49,8 +28,7 @@ def api():
 	if request.method == 'GET':
 		tpr = requests.get(url, headers=headers)
 
-		modded = find_list_resources('img',"src",BeautifulSoup(tpr.content), urllib.parse.urlparse(url).netloc)  
-		return make_response((modded, tpr.status_code, dict(tpr.headers)))
+		return make_response((tpr.content, tpr.status_code, dict(tpr.headers)))
 	# elif request.method == 'HEAD':
 	# 	user = request.form['nm']
 	# 	return redirect(url_for('success',name = user))
