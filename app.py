6c1ba9c2eea45e9da1343d5429ee74c06154d21e
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-22 11:26:59 -1000

    test?

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from typing import Match
 from flask import Flask, render_template, request, flash, make_response
 import requests
 import urllib
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
@@ -16,6 +17,27 @@ def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	return render_template("index.html")
 
+def find_list_resources (tag, attribute,soup, url):
+	for x in soup.findAll(tag):
+		try:
+			x[attribute] = urllib.urljoin(url, x[attribute])
+		except KeyError:
+			pass
+	return soup
+
+# soup = BeautifulSoup(html)
+
+# image_scr = find_list_resources('img',"src",soup)   
+# scipt_src = find_list_resources('script',"src",soup)    
+# css_link = find_list_resources("link","href",soup)
+# video_src = find_list_resources("video","src",soup)         
+# audio_src = find_list_resources("audio","src",soup) 
+# iframe_src = find_list_resources("iframe","src",soup)
+# embed_src = find_list_resources("embed","src",soup)
+# object_data = find_list_resources("object","data",soup)         
+# soruce_src = find_list_resources("source","src",soup)    
+
+
 @app.route("/api")
 def api():
 	url = request.args.get('url')
@@ -26,8 +48,9 @@ def api():
 
 	if request.method == 'GET':
 		tpr = requests.get(url, headers=headers)
-		print(dict(tpr.headers))
-		return make_response((tpr.content, tpr.status_code, dict(tpr.headers)))
+
+		modded = find_list_resources('img',"src",BeautifulSoup(tpr.content), urllib.parse.urlparse(url).netloc)  
+		return make_response((modded, tpr.status_code, dict(tpr.headers)))
 	# elif request.method == 'HEAD':
 	# 	user = request.form['nm']
 	# 	return redirect(url_for('success',name = user))
