bd739419e2c123f2bdddcac82c9ff744f68d0a5b
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-23 11:34:43 -1000

    c

--- app.py
+++ app.py
@@ -63,8 +63,9 @@ def api():
 		tpr = requests.get(url, headers=headers)
 		encodings = dict(tpr.headers)['Content-Encoding'].strip(' ').split(',')
 		for encoding in encodings:
+			print(tpr.content)
 			if encoding == 'br':
-				content =  brotli.compress(proxyHTML(brotli.decompress(tpr.text), urllib.urlparse(url).netloc))
+				content =  brotli.compress(proxyHTML(brotli.decompress(tpr.content), urllib.urlparse(url).netloc))
 			elif encoding == 'gzip':
 				content =  gzip.compress(proxyHTML(gzip.decompress(tpr.content), urllib.urlparse(url).netloc))
 			elif encoding == 'deflate':
