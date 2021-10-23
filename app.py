20aa0dc0bf898e2836ca7defea1f8a3487159504
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-22 16:39:46 -1000

    h

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from typing import Match
 from flask import Flask, render_template, request, flash, make_response
 import requests
-import urllib
+import urllib.parse as urllib
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -17,6 +17,37 @@ def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	return render_template("index.html")
 
+def urlToApi(url, base):
+    if bool(urlparse(url).netloc):
+        return f'https://radford.herokuapp.com/api?url={url}'
+    elif url.startswith('{{') and url.endswith('}}'):
+        return url
+    else:
+        return f'https://radford.herokuapp.com/api?url={urljoin(base, url)}'
+
+def find_list_resources (tag, attribute, soup, domain):
+   for x in soup.findAll(tag):
+       try:
+           x[attribute] = urlToApi(x[attribute], domain)
+       except KeyError:
+           pass
+   return(soup)
+
+def proxyHTML(html, domain):
+    soup = BeautifulSoup(html)
+
+    soup = find_list_resources('img', "src", soup, domain)
+    soup = find_list_resources('script', "src", soup, domain)
+    soup = find_list_resources("link", "href", soup, domain)
+    soup = find_list_resources("video", "src", soup, domain)
+    soup = find_list_resources("audio", "src", soup, domain)
+    soup = find_list_resources("iframe", "src", soup, domain)
+    soup = find_list_resources("embed", "src", soup, domain)
+    soup = find_list_resources("object", "data", soup, domain)
+    soup = find_list_resources("source", "src", soup, domain)
+
+    return soup
+
 @app.route("/api")
 def api():
 	url = request.args.get('url')
@@ -27,8 +58,9 @@ def api():
 
 	if request.method == 'GET':
 		tpr = requests.get(url, headers=headers)
+		content = proxyHTML(tpr.content)
 		
-		return make_response((tpr.content, tpr.status_code, dict(tpr.headers)))
+		return make_response((content, tpr.status_code, dict(tpr.headers)))
 	# elif request.method == 'HEAD':
 	# 	user = request.form['nm']
 	# 	return redirect(url_for('success',name = user))
