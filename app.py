2fbb511ffa433f9ad6b013cf4b5437f002b00840
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-23 14:35:29 -1000

    s

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def greeter():
 
 @app.route("/test")
 def test():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	response = make_response(('<html><head><title>testing</title></head><body><h1>testing 123</h1></body></html', 200))
 	print(type(response))
 	print(response.get_data())
