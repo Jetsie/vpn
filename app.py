adbe1c1c75c701417dedd3804248f4e4229d8836
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-25 22:07:00 -1000

    d

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def urlToApi(url, base):
         return url
     else:
         print(urllib.urljoin(base, url))
-        return f'https://radford.herokuapp.com/api?url={}'
+        return f'https://radford.herokuapp.com/api?url={urllib.urljoin(base, url)}'
         
 
 def find_list_resources (tag, attribute, soup, domain):
