95c1fccea60c4c08f7332aca3fa50984dae3a6b3
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-25 22:27:20 -1000

    d

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def greeter():
     flash("Hi " + str(request.form['name_input']) + ", great to see you!")
     return render_template("index.html")
 
-def urlToApi(url, base):
+def urlToApi(url, base, scheme):
     if bool(urllib.urlparse(url).netloc):
         return f'https://radford.herokuapp.com/api?url={url}'
     elif url.startswith('{{') and url.endswith('}}'):
@@ -26,39 +26,40 @@ def urlToApi(url, base):
         return url
     else:
         link = '/' if not url.startswith('/') else ''
-        return f'https://radford.herokuapp.com/api?url={base}{link}{url}'
+        return f'https://radford.herokuapp.com/api?url={scheme}s{base}{link}{url}'
         
 
-def find_list_resources (tag, attribute, soup, domain):
+def find_list_resources (tag, attribute, soup, domain, scheme):
    for x in soup.findAll(tag):
        try:
-           x[attribute] = urlToApi(x[attribute], domain)
+           x[attribute] = urlToApi(x[attribute], domain, scheme)
        except KeyError:
            pass
    return(soup)
 
-def proxyHTML(html, domain):
+def proxyHTML(html, domain, scheme):
     soup = BeautifulSoup(html, features="html.parser")
 
-    soup = find_list_resources('img', "src", soup, domain)
-    soup = find_list_resources('script', "src", soup, domain)
-    soup = find_list_resources("link", "href", soup, domain)
-    soup = find_list_resources("video", "src", soup, domain)
-    soup = find_list_resources("audio", "src", soup, domain)
-    soup = find_list_resources("iframe", "src", soup, domain)
-    soup = find_list_resources("embed", "src", soup, domain)
-    soup = find_list_resources("object", "data", soup, domain)
-    soup = find_list_resources("source", "src", soup, domain)
+    soup = find_list_resources('img', "src", soup, domain, scheme)
+    soup = find_list_resources('script', "src", soup, domain, scheme)
+    soup = find_list_resources("link", "href", soup, domain, scheme)
+    soup = find_list_resources("video", "src", soup, domain, scheme)
+    soup = find_list_resources("audio", "src", soup, domain, scheme)
+    soup = find_list_resources("iframe", "src", soup, domain, scheme)
+    soup = find_list_resources("embed", "src", soup, domain, scheme)
+    soup = find_list_resources("object", "data", soup, domain, scheme)
+    soup = find_list_resources("source", "src", soup, domain, scheme)
 
     return str(soup)
 
 @app.route("/api")
 def api():
     url = request.args.get('url')
+    urlparse = urllib.urlparse(url)
     headers = dict(request.headers)
     # Edit Host header
     if headers['Host']:
-        headers['Host'] = urllib.urlparse(url).netloc
+        headers['Host'] = urlparse.netloc
 
     if request.method == 'GET':
         try:
@@ -67,7 +68,7 @@ def api():
             abort(404)
         # encodings = dict(tpr.headers)['Content-Encoding'].strip(' ').split(',')
         # print(tpr.content)
-        content = proxyHTML(tpr.content, urllib.urlparse(url).netloc)
+        content = proxyHTML(tpr.content, urlparse.netloc, urlparse.scheme)
         response = make_response((content, tpr.status_code))
         return response
     # elif request.method == 'HEAD':
