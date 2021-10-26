d14f2535077dcc89029bb673785631360beb8117
Jet <84949210+Jetsie@users.noreply.github.com>
2021-10-25 21:49:04 -1000

    d

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ app.secret_key = "manbearpig_MUDMAN888"
 
 @app.route("/")
 def index():
-	flash("what's your name?")
-	return render_template("index.html")
+    flash("what's your name?")
+    return render_template("index.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+    return render_template("index.html")
 
 def urlToApi(url, base):
     if bool(urllib.urlparse(url).netloc):
         return f'https://radford.herokuapp.com/api?url={url}'
     elif url.startswith('{{') and url.endswith('}}'):
         return url
-	elif url.startswith('data:'):
-		return url
+    elif url.startswith('data:'):
+        return url
     else:
         return f'https://radford.herokuapp.com/api?url={urllib.urljoin(base, url)}'
 
@@ -52,45 +52,45 @@ def proxyHTML(html, domain):
 
 @app.route("/api")
 def api():
-	url = request.args.get('url')
-	headers = dict(request.headers)
-	# Edit Host header
-	if headers['Host']:
-		headers['Host'] = urllib.urlparse(url).netloc
+    url = request.args.get('url')
+    headers = dict(request.headers)
+    # Edit Host header
+    if headers['Host']:
+        headers['Host'] = urllib.urlparse(url).netloc
 
-	if request.method == 'GET':
-		try:
-			tpr = requests.get(url, headers=headers)
-		except:
-			abort(404)
-		# encodings = dict(tpr.headers)['Content-Encoding'].strip(' ').split(',')
-		# print(tpr.content)
-		content = proxyHTML(tpr.content, urllib.urlparse(url).netloc)
-		response = make_response((content, tpr.status_code))
-		return response
-	# elif request.method == 'HEAD':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	# elif request.method == 'POST':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	# elif request.method == 'PUT':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	# elif request.method == 'DELETE':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	# elif request.method == 'CONNECT':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	# elif request.method == 'OPTIONS':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	# elif request.method == 'TRACE':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	# elif request.method == 'PATCH':
-	# 	user = request.form['nm']
-	# 	return redirect(url_for('success',name = user))
-	
-	
+    if request.method == 'GET':
+        try:
+            tpr = requests.get(url, headers=headers)
+        except:
+            abort(404)
+        # encodings = dict(tpr.headers)['Content-Encoding'].strip(' ').split(',')
+        # print(tpr.content)
+        content = proxyHTML(tpr.content, urllib.urlparse(url).netloc)
+        response = make_response((content, tpr.status_code))
+        return response
+    # elif request.method == 'HEAD':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    # elif request.method == 'POST':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    # elif request.method == 'PUT':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    # elif request.method == 'DELETE':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    # elif request.method == 'CONNECT':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    # elif request.method == 'OPTIONS':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    # elif request.method == 'TRACE':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    # elif request.method == 'PATCH':
+    # 	user = request.form['nm']
+    # 	return redirect(url_for('success',name = user))
+    
+    
